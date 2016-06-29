7941d0a66c44a730f240cf78b21949ab9f115ab3
Steven Fine <sfine@google.com>
2016-06-29 15:20:56 -0700

    Add event listener for when content_script.js replies back to the extension

--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
-function click() {
-  	chrome.tabs.executeScript(null, {file: "content_script.js"}, function(result) {
-  		var a = document.getElementById('download');
-  		a.href = result[0];
-  		a.download = "webpage.html";
-  		a.innerHTML = "Download";
-  	});
-  	// window.close();
-}
-
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('button').addEventListener('click', click);
+});
+
+function click() {
+  chrome.tabs.executeScript(null, {file: "content_script.js"});
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  		var a = document.getElementById('download');
+  		a.href = request;
+  		a.download = "webpage.html";
+  		a.innerHTML = "Download";
 });
