d71ec71a4dadb1060062ea89199f8c9f6b4b7fba
Steven Fine <sfine@google.com>
2016-07-12 16:40:13 -0700

    Significant changes cleaning classes and adding functionality

--- content_script.js
+++ content_script.js
@@ -1,95 +1,213 @@
-var FILTERED_TAGS = new Set(['script', 'noscript', 'style', 'link']);
+/**
+ * HTML serializer that takes a document and synchronously stores it as an array of strings and stores enough state to later
+ * asynchronously convert it into an html text file.
+ */
+class HTMLSerializer { // TODO: BECAUSE MULTIPLE CONTENT SCRIPTS CAN BE INJECTED, GIVES Identifier 'HTMLSerializer' has already been declared ERROR
+                       //       AND IF I ENCAPSULATE THE CLASS IN {} THE TESTS WONT BE ABLE TO INSTANTIATE AN INSTANCE
+  constructor() {
 
-function serializeDocument(html, srcHoles, frameHoles, doc, depth) {
-	html.push('<!DOCTYPE html>\n');
-	var node = doc.firstChild;
-	while (node.nodeType == Node.DOCUMENT_TYPE_NODE) {
-		node = node.nextSibling; // could this cause an infinite loop?
-	}
-	serializeTree(html, srcHoles, frameHoles, node, depth);
-}
+    /**
+     * @private {Set<string>} Contains the lower case tag names that should be ignored while serializing a document.
+     * @const
+     */
+    this.FILTERED_TAGS = new Set(['script', 'noscript', 'style', 'link']);
 
-function serializeTree(html, srcHoles, frameHoles, element, depth) {
-  if (!element.tagName && element.nodeType != Node.TEXT_NODE) {
-    // ignore elements that don't have tags and are not text
-  } else if (element.tagName && FILTERED_TAGS.has(element.tagName.toLowerCase())) {
-    // filter out elements that are in filteredTags
-  } else if (element.nodeType == Node.TEXT_NODE) {
-    html.push(element.textContent);
-  } else {
-    html.push(new Array(depth+1).join('  '));
-    html.push(`<${element.tagName.toLowerCase()} `);
+    /**
+     * @public {Array<string>} This array represents the serialized html that makes up an element or document. 
+     */
+    this.html = [];
 
-    var style = element.ownerDocument.defaultView.getComputedStyle(element, null).cssText;
-    style = style.replace(/"/g, "'");
-    html.push(`style="${style}" `);
+    /**
+     * @public {Object<number, string>} The keys represent an index in this.html.  The value
+     *    is a url at which the resource that belongs at that index can be retrieved.  The resource will eventually
+     *    be converted to a data url.  Because any given document being serialized could be an iframe which is
+     *    nested 1 or more levels into the root document, the exact quotes that will be used to surround the data url
+     *    must be determined when the frames are being put together, so that they can be properly escaped.  As a
+     *    result, this.html has a filler index both immediately before, and immediately after where the data url will
+     *    be placed (3 filler indices in total).
+     */
+    this.srcHoles = {};
 
-    var attributes = element.attributes;
-    if (attributes) {
-      for (var i = 0; i < attributes.length; i++) { // TODO: change this to the proper kind of loop
-        var attribute = attributes[i];
-        if (attribute.name.toLowerCase() == 'src') {
-        	if (element.tagName.toLowerCase() != 'iframe') {
-        		html.push(`${attribute.name}="`);
-        		srcHoles[html.length] = attribute.value; // mark location to insert url later
-        		html.push(''); // create filler entry
-        		html.push('" ');
-        	} else {
-        		html.push('srcdoc=');
-        		frameHoles[html.length] = attribute.value;
-        		html.push(''); // create filler entry
-        	}
-        } else if (attribute.name.toLowerCase() != "style") {
-          html.push(`${attribute.name}="${attribute.value}" `);
+    /**
+     * @public {Object<number, string>} The keys represent an index in this.html.  The value
+     *    is a string that uniquely identifies an iframe, the serialized contents of which should be placed at that
+     *    index of this.html.
+     */
+    this.frameHoles = {};
+
+    /**
+     * @public {Array<number>} Each number in this.styleIndices corresponds to an index in this.html at which a
+     *    serialized style attribute is located. This is because there are styles that contain quotation marks.
+     *    Because any given document being serialized could be an iframe which is nested 1 or more levels into
+     *    the root document, the exact quotes that will be used must be determined when the frames are being put
+     *    together, so that they can be properly escaped.
+     */
+    this.styleIndices = [];
+  }
+
+  /**
+   * Takes an html element, and populates this object's fields such that it can eventually be converted into an
+   * html text file.
+   *
+   * @param {Element} element The Element to serialize
+   * @param {number} depth The number of parents this element has in the current frame
+   * @private
+   */ 
+  serializeTree(element, depth) {
+    if (!element.tagName && element.nodeType != Node.TEXT_NODE) {
+      // ignore elements that don't have tags and are not text
+    } else if (element.tagName && this.FILTERED_TAGS.has(element.tagName.toLowerCase())) {
+      // filter out elements that are in filteredTags
+    } else if (element.nodeType == Node.TEXT_NODE) {
+      this.html.push(element.textContent);
+    } else {
+      this.html.push(new Array(depth+1).join('  '));
+      this.html.push(`<${element.tagName.toLowerCase()} `);
+
+      var style = element.ownerDocument.defaultView.getComputedStyle(element, null).cssText;
+      this.styleIndices.push(this.html.length);
+      this.html.push(`style="${style}" `);
+
+      var attributes = element.attributes;
+      if (attributes) {
+        for (var i = 0, attribute; attribute = attributes[i]; i++) {
+          if (attribute.name.toLowerCase() == 'src') {
+            if (element.tagName.toLowerCase() != 'iframe') {
+              this.html.push(`${attribute.name}=`);
+              this.html.push(''); // entry where properly escaped quotes will go
+              this.srcHoles[this.html.length] = attribute.value; // mark location to insert url later
+              this.html.push(''); // entry where data url will go
+              this.html.push(''); // entry where properly escaped quotes will go
+              this.html.push(' ');
+            }
+          } else if (attribute.name.toLowerCase() != 'style') {
+            this.html.push(`${attribute.name}="${attribute.value}" `);
+          }
+        }
+        if (element.tagName.toLowerCase() == 'iframe') { // TODO: IS IT POSSIBLE AN IFRAME CAN HAVE NO ATTRIBUTES?
+          this.html.push('srcdoc=');
+          this.frameHoles[this.html.length] = this.iframeFullPosition(window) + '.' + this.iframePosition(element.contentWindow);
+          this.html.push(''); // entry where the iframe contents will go
+          this.html.push(' ');
         }
       }
-      html.push('>\n');
-    }
-    
-    var children = element.childNodes;
-    if (children) {
-      for (var i = 0; i < children.length; i++) { // TODO: change this to the proper kind of loop
-        var child = children[i];
-        serializeTree(html, holes, child, depth+1);
-      }
-    }
 
-    html.push(new Array(depth+1).join('  '));
-    html.push(`</${element.tagName.toLowerCase()}>\n`);
+      this.html.push('>\n');
+
+      var children = element.childNodes;
+      if (children) {
+        for (var i = 0, child; child = children[i]; i++) {
+          this.serializeTree(child, depth+1);
+        }
+      }
+
+      this.html.push(new Array(depth+1).join('  '));
+      this.html.push(`</${element.tagName.toLowerCase()}>\n`);
+    }
+  }
+
+  /**
+   * Takes an html document, and populates this objects fields such that it can eventually be converted into an
+   * html file.
+   *
+   * @param {Document} doc The Document to serialize. Defaults to the current document
+   * @public
+   */ 
+  serializeDocument(doc) {
+    doc = doc || document;
+    this.html.push('<!DOCTYPE html>\n');
+    var node = doc.firstChild;
+    while (node.nodeType == Node.DOCUMENT_TYPE_NODE) {
+      node = node.nextSibling; // TODO: could this cause an infinite loop?
+    }
+    this.serializeTree(node, 0);
+  }
+
+  /**
+   * Computes the index of the window in it's parent's array of frames.
+   *
+   * @param {Window} win The window to use in the calculation
+   * @return {number} the frames index
+   */
+  iframePosition(win) {
+    if (win.parent != win) {
+      for (var i = 0; i < win.parent.frames.length; i++) {
+        if (win.parent.frames[i] == win) {
+          return i;
+        }
+      }
+    } else {
+      return -1;
+    }
+  }
+
+  /**
+   * Computes the full path of the frame in the root document.  Nested layers are seperated by '.'
+   *
+   * @param {Window} win The window to use in the calculation
+   * @return {string} The full path
+   */
+  iframeFullPosition(win) {
+    if (this.iframePosition(win) < 0) {
+      return '0';
+    } else {
+      return this.iframeFullPosition(win.parent) + '.' + this.iframePosition(win); 
+    }
   }
 }
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
-function fillHoles(html, holes) {
-  if (Object.keys(holes).length == 0) {
-    var output = html.join('');
-    var file = new Blob([output], {type: 'text/html'});
-    var url = URL.createObjectURL(file)
-    // var extensionId = "nlhglfcihneopknjknodakgjfiapnnmn";
-    chrome.runtime.sendMessage(url);
-  }
-  else {
-    var index = Object.keys(holes)[0];
-    var src = holes[index];
-    delete holes[index];
+/**
+ * Takes all of the srcHoles in the HTMLSerializer starting at index i, and creates data
+ * urls for the resources, and places them in this.html
+ *
+ * @param {HTMLSerializer} htmlSerializer The HTMLSerializer
+ * @param {number} i The index of this.srcHoles at which to start
+ * @param {Function} callback The callback function
+ */
+function fillSrcHoles(htmlSerializer, i, callback) {
+  if (i == Object.keys(htmlSerializer.srcHoles).length) {
+    callback(htmlSerializer);
+  } else {
+    var index = Object.keys(htmlSerializer.srcHoles)[i];
+    var src = htmlSerializer.srcHoles[index];
     fetch(src).then(function(response) {
       return response.blob();
       }).then(function(blob) {
       var reader = new FileReader();
       reader.onload = function(e) {
-        html[index] = '"' + e.target.result + '" ';
-        fillHoles(html, holes);
+        htmlSerializer.html[index] = e.target.result;
+        fillSrcHoles(htmlSerializer, i+1, callback);
       }
       reader.readAsDataURL(blob)
     }).catch(function(error) {
       console.log(error);
-      // html[index] = '"" ';
-      fillHoles(html, holes);
+      fillSrcHoles(htmlSerializer, i+1, callback);
     });
   }
 }
 
-var srcHoles = {};
-var frameHoles = {};
-var html = [];
-serializeDocument(html, srcHoles, frameHoles, document, 0);
-fillHoles(html, holes);
+/**
+ * Send the neccessary HTMLSerializer properties back to the extension
+ *
+ * @param {HTMLSerializer} htmlSerializer The HTMLSerializer
+ */
+function sendHTMLSerializerToExtension(htmlSerializer) {
+  var result = {
+    'html': htmlSerializer.html,
+    'srcHoles': htmlSerializer.srcHoles,
+    'frameHoles': htmlSerializer.frameHoles,
+    'styleIndices': htmlSerializer.styleIndices,
+    'frameIndex': htmlSerializer.iframeFullPosition(window)
+  };
+  chrome.runtime.sendMessage(result);
+}
+
+
+/////////// TODO: CHECK FOR TEST IN A BETTER WAY /////////////
+if (typeof IS_TEST === typeof undefined || !IS_TEST) {
+  var htmlSerializer = new HTMLSerializer();
+  htmlSerializer.serializeDocument();
+  fillSrcHoles(htmlSerializer, 0, sendHTMLSerializerToExtension)
+}
--- popup.js
+++ popup.js
@@ -3,12 +3,101 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function click() {
-  chrome.tabs.executeScript(null, {file: 'content_script.js', allFrames: true});
+  var messages = []
+  var results;
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    messages.push(message);
+    if (results && messages.length == results.length) {
+      completeProcess(messages);
+    }
+  });
+
+  chrome.tabs.executeScript(null, {file: 'content_script.js', allFrames: true}, function(response) { // TODO: FIGURE OUT WHY NOT ALL IFRAMES ARE GETTING CONTENT SCRIPTS INJECTED
+    results = response;
+    if (messages.length == results.length) {
+      completeProcess(messages);
+    }
+  });
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-      var a = document.getElementById('download');
-      a.href = request;
-      a.download = 'webpage.html';
-      a.innerHTML = 'Download';
-});
+/**
+ * Takes all the responses from the injected content scripts and creates the HTML file for download
+ * 
+ * @param {Array<Object>} messages The response from all of the injected content scripts.
+ */
+function completeProcess(messages) {
+  var html = outputHTMLString(messages);
+  var file = new Blob([html], {type: 'text/html'});
+  var url = URL.createObjectURL(file);
+
+  var a = document.getElementById('download');
+  a.href = url;
+  a.download = "webpage.html";
+  a.innerHTML = "Download";
+}
+
+/**
+ * Converts the responses from the injected content scripts into a string representing the HTML.
+ * 
+ * @param {Array<Object>} messages The response from all of the injected content scripts.
+ * @return {string} The resulting HTML
+ */
+function outputHTMLString(messages) {
+  var rootIndex = 0;
+  for (var i = 1; i < messages.length; i++) {
+    rootIndex = messages[i].frameIndex === '0' ? i : rootIndex;
+  }
+  fillRemainingHoles(messages, rootIndex, 0);
+  return messages[rootIndex].html.join('');
+}
+
+/**
+ * Fills all of the gaps in messages[i].html
+ *
+ * @param {Array<Object>} messages The response from all of the injected content scripts
+ * @param {number} i The index of messages to use
+ * @param {number} depth How many parent iframes messages[i] has
+ */
+function fillRemainingHoles(messages, i, depth) {
+  var html = messages[i].html;
+  var quotes = getQuotes(depth);
+  var frameHoles = messages[i].frameHoles;
+  for (var index in frameHoles) {
+    if (frameHoles.hasOwnProperty(index)) {
+      var frameIndex = frameHoles[index];
+      for (var j = 0; j < messages.length; j++) {
+        if (messages[j].frameIndex == frameIndex) {
+          fillRemainingHoles(messages, j, depth+1);
+          html[index] = quotes + messages[j].html.join('') + quotes;
+        }
+      }
+    }
+  }
+  var srcHoles = messages[i].srcHoles;
+  for (var index in srcHoles) {
+    if (srcHoles.hasOwnProperty(index)) {
+      var srcIndex = srcHoles[index];
+      html[srcIndex-1] = quotes;
+      html[srcIndex+1] = quotes;
+    }
+  }
+  var styleIndices = messages[i].styleIndices;
+  for (var i = 0; i < styleIndices.length; i++) {
+    var style = html[styleIndices[i]];
+    html[styleIndices[i]] = style.replace(/"/g, quotes);
+  }
+}
+
+/**
+ * Calculate the correct quotes that should be used given how many parent iframes a given frame has
+ *
+ * @param {number} depth The number of parent iframes
+ * @return {string} The correctly escaped quotation marks
+ */
+function getQuotes(depth) {
+  if (depth == 0) {
+    return '"';
+  } else {
+    return '&' + new Array(depth).join('amp;') + 'quot;';
+  }
+}
