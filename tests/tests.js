8a428ff5d8f03b8ddbab001d0f470cec3763532e
Steven Fine <sfine@google.com>
2016-07-13 15:22:16 -0700

    Change serializeTree to processTree

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,10 @@
-QUnit.test("serializeTree with a single node", function(assert) {
+QUnit.test("processTree with a single node", function(assert) {
   var fixture = document.getElementById('qunit-fixture');
   var node = document.createElement('div');
   node.appendChild(document.createTextNode('test'));
   fixture.appendChild(node);
   var html = new HTMLSerializer();
-  html.serializeTree(node, 0);
+  html.processTree(node, 0);
   assert.equal(0, Object.keys(html.srcHoles).length);
   assert.equal(0, Object.keys(html.frameHoles).length);
   assert.equal(1, html.styleIndices.length);
